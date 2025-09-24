79a1afc6109f2029762ede834d12f68c8c7718fa
Aarfdark <aadarsh.natarajan@gmail.com>
2025-09-23 20:00:23 -0400

    .

--- Source/Scripts/noise.gd
+++ Source/Scripts/noise.gd
@@ -9,7 +9,6 @@ func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Enemy"):
 		body.change_state(body.HUNTING)
 
-
 func _on_body_exited(body: Node3D) -> void:
 	if body.is_in_group("Enemy"):
 		body.change_state(body.SEARCHING)
