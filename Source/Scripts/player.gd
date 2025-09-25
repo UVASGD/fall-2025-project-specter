ebd494a997d0472a7241a4a20f02871fff5d8816
Momen <momenabdelmoaty@gmail.com>
2025-09-25 04:48:58 -0400

    raycasting for sound prototype

--- Source/Scripts/player.gd
+++ Source/Scripts/player.gd
@@ -11,11 +11,14 @@ extends CharacterBody3D
 @onready var stamina_timer = $StaminaTimer
 @onready var hud = %HUD
 @onready var noise_area = $Area3D/CollisionShape3D.shape
+@onready var space_state = get_world_3d().direct_space_state
 var crouch = false
 var recovering = false
 var holding_breath = false
 var stamina = 100.0
 var noise_level : float
+var detections = {}
+var soundTimer = 0;
 
 enum {IDLE, CROUCH, CROUCH_SPRINT, WALK, SPRINT}
 
@@ -23,6 +26,12 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
+	if soundTimer == 50:
+		soundTimer=0
+		causeSound()
+	else:
+		soundTimer+=1
+	
 	var used_stamina = false
 	noise_level = 0.0
 	
@@ -135,18 +144,87 @@ func _physics_process(delta: float) -> void:
 	if stamina < 0:
 		stamina = 0
 		holding_breath = false
+	
+	if Input.is_action_just_pressed("testsoundray"):
+		causeSound()
+		
+func causeSound():
+	#var startingpoint = global_transform.origin
+		#var endingpoint = startingpoint + -global_transform.basis.z * 1000
+		#fire_ray(startingpoint, endingpoint, 25, [self])
+		var visuals = false
+		var duration = 10000
+		
+		var num_rays=32
+		var num_levels = 3
+		var startingpoint = global_transform.origin + Vector3(0, .25, 0)
+		for j in range(num_levels):
+			for i in range (num_rays):
+				var angle = deg_to_rad((360/num_rays)*i)
+				var pitchAngle = deg_to_rad(135+45*j)
+				var rayDirection = Vector3(sin(angle)*cos(pitchAngle), sin(pitchAngle), cos(angle))
+				var endingpoint = startingpoint + rayDirection.normalized() * 100
+				fire_ray(startingpoint, endingpoint, 100, [self], visuals, false, duration)
+		print("EndOfShootingRays")
+		detections.clear()
+		
+func fire_ray(start, end, power, exclude, visuals, throughWall, duration):
+	var rayQuery = PhysicsRayQueryParameters3D.create(start, end)
+	rayQuery.exclude=exclude
+	var rayResult = space_state.intersect_ray(rayQuery)
+	if rayResult:
+		if visuals:
+			if throughWall==false:
+				DebugDraw3D.draw_line(start, rayResult.position, Color(1, 1, 0), duration)
+			else:
+				DebugDraw3D.draw_line(start,  rayResult.position, Color(0, 1, 0), duration)
+		#print("Ray hit: ", rayResult.collider.name, " at ", rayResult["rid"])
+		
+		var currentParentNode = rayResult.collider
+		var hitEnemy = null
+		while currentParentNode:
+			if currentParentNode.has_method("get_signal"):
+				print("Hit Enemy: ", currentParentNode.name)
+				hitEnemy = currentParentNode
+				break
+			else:
+				currentParentNode=currentParentNode.get_parent()
+			
+		if hitEnemy==null and power>15:
+			var incomingAngle = (rayResult.position-start).normalized()
+			var normalAngle = rayResult.normal.normalized()
+			var newAngle = (incomingAngle-2 * incomingAngle.dot(normalAngle) * normalAngle).normalized()
+			var reflectionStart = rayResult.position+newAngle*0.05
+			fire_ray(reflectionStart,reflectionStart+(newAngle*100), power*(3.0/5), [self], visuals, throughWall, duration)
+			var throughWallStart=rayResult.position+incomingAngle*0.05
+			fire_ray(throughWallStart,throughWallStart+(incomingAngle*100), power*(1.0/5), [self], visuals, true, duration)
+	else:
+		#print("Ray hit nothing")
+		if visuals:
+			if throughWall==false:
+				DebugDraw3D.draw_line(start, end, Color(1, 0, 0), duration)
+			else:
+				DebugDraw3D.draw_line(start, end, Color(0, 0, 1), duration)
+
+
+	
+	
+
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 	###DEBUGGING ONLY
+	
 	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_ESCAPE:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 
 
 func _on_stamina_timer_timeout() -> void:
