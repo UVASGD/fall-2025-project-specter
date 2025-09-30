9d990c446e0ebfb6231fae65af21209801e41a6a
PurportedFish <purportedfish@gmail.com>
2025-09-30 19:46:48 -0400

    Add overwriteable functions to InteractableLever class

--- Source/Scripts/Interactable_lever.gd
+++ Source/Scripts/Interactable_lever.gd
@@ -2,16 +2,21 @@ class_name InteractableLever
 extends Interactable
 
 
-signal lever_down
-signal lever_up
-
 var is_down: bool = false
 
 
 func interact(_player: Player) -> void:
 	if is_down:
-		lever_up.emit()
+		lever_pulled_down()
 		is_down = false
 	else:
-		lever_down.emit()
+		lever_pulled_up()
 		is_down = true
+
+
+func lever_pulled_down() -> void:
+	pass
+
+
+func lever_pulled_up() -> void:
+	pass
