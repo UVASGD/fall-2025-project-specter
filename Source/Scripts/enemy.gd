3b198160c7ff93207e3c1368b7502f88f724b970
viamill <126583359+viamill@users.noreply.github.com>
2025-10-16 18:16:04 -0400

    fuck my drag

--- Source/Scripts/enemy.gd
+++ Source/Scripts/enemy.gd
@@ -12,9 +12,28 @@ enum {IDLE, ROAMING, SEARCHING, HUNTING}
 @onready var rc = $RayCast3D
 var target_pos : Vector3
 var search_pos : Vector3
+var menace_gauges = []
 var SPEED = 5
 
+class menace_gauge:
+	var value : float
+	var pos : Vector3
+	
+	func _init(noise_level, noise_position) -> void:
+		value = (noise_level * 10)
+		pos = noise_position
+	
+	func increase(num, new_pos = null):
+		value += num
+		if value > 100: value = 100
+		if new_pos: pos = new_pos
+	
+	func reduce(num) -> bool:
+		value -= num
+		return value > 0
+
 func _physics_process(_delta: float) -> void:
+	get_room_points(AABB(Vector3(0,0,0), Vector3(150, 1, 150)), 10, 1.0)
 	match current_state:
 		IDLE:
 			velocity = Vector3.ZERO
@@ -117,3 +136,42 @@ func echolocate():
 			set_search_point()
 			target_pos = player.global_position
 		rc.set_enabled(false)
+
+func hear_sound(noise_level : float, pos : Vector3):
+	var to_delete = []
+	for i in range(menace_gauges.size()):
+		if !menace_gauges[i].reduce(10): to_delete.append(i)
+	for i in to_delete:
+		menace_gauges.erase(i)
+	
+	menace_gauges.append(menace_gauge.new(noise_level, pos))
+
+func get_room_points(area_aabb: AABB, max_attempts = 10, tolerance = 1.0):
+	var RADIUS = 15
+	var output = []
+	var t = Time.get_ticks_usec()
+	
+	var nav_map = nav_agent.get_navigation_map()
+	var num_cycles = ceil((area_aabb.size.x * area_aabb.size.z) / ((2*RADIUS) ** 2))
+	for i in range(num_cycles):
+		for j in range(max_attempts):
+			var rand_x = randf_range(area_aabb.position.x, area_aabb.position.x + area_aabb.size.x)
+			var rand_z = randf_range(area_aabb.position.z, area_aabb.position.z + area_aabb.size.z)
+			
+			var test_point = Vector3(rand_x, area_aabb.position.y, rand_z)
+			var nav_point = NavigationServer3D.map_get_closest_point(nav_map, test_point)
+			
+			var too_close = false
+			if output.size() > 0:
+				too_close = true
+				for p in output:
+					if nav_point.distance_to(p) > tolerance:
+						too_close = false
+						break
+				
+			if !too_close and Vector2(nav_point.x, nav_point.z).distance_to(Vector2(rand_x, rand_z)) < tolerance:
+				output.append(nav_point)
+				break;
+	
+	print(num_cycles, " ", output.size(), " ", (Time.get_ticks_usec() - t))
+	return output
