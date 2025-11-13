f1ebc89ba31f1fc64d6b3e837494fa2f11f7d4e1
Aarfdark <aadarsh.natarajan@gmail.com>
2025-11-13 18:19:22 -0500

    door opening should work

--- Source/Scripts/door.gd
+++ Source/Scripts/door.gd
@@ -1,7 +1,9 @@
 extends AnimatableBody3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var can_open = true
 
 func open():
-	print("door opening")
-	animation_player.play("door_open")
-	
+	if(can_open):
+		print("door opening")
+		animation_player.play("door_open")
+		can_open = false
