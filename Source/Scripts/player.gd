c8b533d1ca622d30f69e0373314490543db28cca
PurportedFish <purportedfish@gmail.com>
2025-12-06 20:24:07 -0500

    Add dialogue box
    To start a dialogue box use player.start_dialogue.emit(text_array).
    Use type hints for the text array. It must be of type Array[String].
    
    Run TestRange Scene *locally* to look at examples for dialogue box
    appearing on entering an Area3D or interacting with an Interactable

--- Source/Scripts/player.gd
+++ Source/Scripts/player.gd
@@ -2,6 +2,8 @@ class_name Player
 extends CharacterBody3D
 
 
+signal start_dialogue(text_array: Array[String])
+
 enum {IDLE, CROUCH, CROUCH_SPRINT, WALK, SPRINT, READING, LOOKING_AT_DISPLAY, DIALOGUE}
 
 # for debug
@@ -22,7 +24,7 @@ func _enter_tree():
 @onready var hand: Node3D = $Head/Camera3D/Hand
 @onready var grab_hand: Marker3D = $Head/Camera3D/GrabHand
 @onready var stamina_timer = $StaminaTimer
-@onready var hud = %HUD
+@onready var hud: Hud = %HUD
 
 @onready var bflyscene:PackedScene = preload("res://Source/Scenes/bfly.tscn")
 @onready var throwable_scene: PackedScene = preload("res://Source/Scenes/thrown_rock.tscn")
@@ -39,8 +41,11 @@ var eggtimer = 0
 var has_throwable: bool = false
 var door: RigidDoor = null
 
+var _prev_state
+
 
 func _ready():
+	hud.stop_dialogue.connect(_on_stop_dialogue)
 	if not debug_topdown_mode:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -195,9 +200,8 @@ func make_noise(noise_val):
 
 func _unhandled_input(event: InputEvent) -> void:
 	if movement_state == DIALOGUE:
-		#TODO: iterate through dialogue
 		if event.is_action_pressed("interact"):
-			pass
+			hud.get_next_dialogue_text()
 		return
 	
 	if event is InputEventMouseMotion and not movement_state == LOOKING_AT_DISPLAY and not debug_topdown_mode:
@@ -252,6 +256,16 @@ func stop_looking_at_display() -> void:
 
 func _on_stamina_timer_timeout() -> void:
 	recovering = true
+
+
+func _on_start_dialogue(text_array: Array[String]) -> void:
+	_prev_state = movement_state
+	movement_state = DIALOGUE
+	hud.start_dialogue.emit(text_array)
+
+
+func _on_stop_dialogue() -> void:
+	movement_state = _prev_state
 
 
 func contains_subarray(main_array: Array, sub_array: Array) -> bool:
